3d872cfa8c4d0585354836d977d758861fc485db
Evangeline Mae Corcoran <82043547+evangeline-corcoran@users.noreply.github.com>
2024-10-02 16:50:37 +0100

    Update validate-baskerville.sh
    add wandb api key, keep up to date with changes implement in model template

--- validate-baskerville.sh
+++ validate-baskerville.sh
@@ -6,7 +6,7 @@
 #SBATCH --tasks-per-node 1
 #SBATCH --gpus 1
 #SBATCH --cpus-per-gpu 36
-#SBATCH --time 45:00:00
+#SBATCH --time 18:00:00
 #SBATCH --job-name OpticalFlowFarneback-validation
 
 # drop into baskerville
@@ -16,12 +16,18 @@ module load bask-apps/live
 module load Python/3.11.3-GCCcore-12.3.0
 cd /bask/projects/v/vjgo8416-climate/shared/cloudcasting-validation
 
+# set wandb credentials
+export WANDB_API_KEY=f54a416d3367861e299350d1c50ebd22904ffb40
+
 # check if repo exists
-if [ ! -d "ocf-optical-flow" ]; then
+if [ ! -d "ocf-optical-flow-tvl1" ]; then
     echo "Repo does not exist; cloning..."
     git clone https://github.com/alan-turing-institute/ocf-optical-flow.git
 fi
 cd ocf-optical-flow
+
+# ensure that we have the latest changes
+git pull
 
 # create virtual environment if it doesn't exist
 if [ ! -d ".venv" ]; then
