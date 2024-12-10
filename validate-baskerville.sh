6c3ec25f0cc8d69c4ed5bdbec5b2e226d8fcdfdd
Isabel Fenton <IFenton@users.noreply.github.com>
2024-12-10 15:08:04 +0000

    Extending baskerville request time

--- validate-baskerville.sh
+++ validate-baskerville.sh
@@ -6,7 +6,7 @@
 #SBATCH --tasks-per-node 1
 #SBATCH --gpus 1
 #SBATCH --cpus-per-gpu 36
-#SBATCH --time 18:00:00
+#SBATCH --time 45:00:00
 #SBATCH --job-name OpticalFlowFarneback-validation
 
 # drop into baskerville
